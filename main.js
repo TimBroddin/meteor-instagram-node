c1ea48f3ed2f5edc0a0115a6c6d397802c40310f
Tim Broddin <tim@wannabes.be>
2014-05-16 10:19:59 +0200

    debug keys

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 ig = Npm.require('instagram-node').instagram();
+console.log(Meteor.settings);
 if (keys = Meteor.settings['instagram-node']) {
+    console.log(keys);
     ig.use({
         client_id: keys.client_id,
         client_secret: keys.client_secret
